bf82a0fbcd8556511f65a5b9dfa295f4e5b758e1
Josh <wiizrdz@gmail.com>
2018-06-01 02:03:40 -0400

    idek

--- app/src/main/java/com/bragg/tagg/MainActivity.java
+++ app/src/main/java/com/bragg/tagg/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Songs");
 
         recyclerView = findViewById(R.id.recyclerView);
 
