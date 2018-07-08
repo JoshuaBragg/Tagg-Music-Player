c36f3fc5d4b61ba46310294ae3caa5ce58d3a053
Josh <wiizrdz@gmail.com>
2018-07-07 20:33:06 -0400

    Keyboard now automatically appears when you are adding tagg

--- app/src/main/java/gg/joshbra/tagg/Activities/TaggActivity.java
+++ app/src/main/java/gg/joshbra/tagg/Activities/TaggActivity.java
@@ -1,5 +1,7 @@
 package gg.joshbra.tagg.Activities;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -20,6 +22,7 @@ import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -114,6 +117,11 @@ public class TaggActivity extends AppCompatActivity implements NavigationView.On
                     public void onClick(View view) {
                         final EditText newTaggEditText = new EditText(view.getContext());
                         newTaggEditText.setTextColor(getResources().getColor(R.color.colorTextSecondary));
+
+                        newTaggEditText.requestFocus();
+
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
                         final AlertDialog alert = new AlertDialog.Builder(view.getContext(), R.style.Dialog)
                                 .setView(newTaggEditText)
